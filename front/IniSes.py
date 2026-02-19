f04f3989120dd9775c720e4217eff923967f9ffd
DerShadow960 <0273680@up.edu.mx>
2026-02-19 00:38:04 +0000

    first commit

---
+++ front/IniSes.py
@@ -0,0 +1,61 @@
+import tkinter.messagebox as msg
+import tkinter as tk
+import socket
+
+
+
+def goconn_upload_pinfo():
+    # Creamos el socket para hablar con el backend
+    # socket.AF_INET--- Comando usado para indicar el uso del protocolo de internet version 4
+    # socket.SOCK_STREAM--- Tipo de socket que usa el protoco TCP (Transmision Control Protocol), 
+    #                           se usa para la transmicion de datos, de forma segura y estable
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect(('localhost', 8080))
+    
+    # Enviamos los datos del cuadro de texto
+    mensaje = f"{entry_nombre.get()}, {entry_monto.get()}"
+    cliente.send(mensaje.encode())
+    
+    # Recibimos confirmación
+    respuesta = cliente.recv(1024)
+    cliente.close()
+
+def loadgame():
+    res = msg.askyesno("¿Continuar?", "¿Los datos estan correctos?")
+    if res:
+        app.withdraw()
+        gamecon = tk.Toplevel()
+        gamecon.title("Menu de juegos")
+        gamecon.geometry("500x300+100+100")
+    elif not res:
+        dev = -1 
+    return dev
+
+def shutdown():
+    s = msg.askyesno("¿TE VAS?", "¿Me abandonas?")
+    if s:
+        ret = 1
+    elif not s:
+        ret = -1
+    return ret
+
+app = tk.Tk()
+app.title("Cliente Casino")
+app.geometry("500x300+100+100")
+
+
+l_nombre = tk.Label(app, text="Nombre:").pack()
+pnom = entry_nombre = tk.Entry(app)
+entry_nombre.pack()
+
+l_monto = tk.Label(app, text="Monto a iniciar:").pack()
+pmon = entry_monto = tk.Entry(app)
+entry_monto.pack()
+
+tk.Button(app, text="Nueva Partida", command= goconn_upload_pinfo).pack()
+tk.Button(app, text="Cargar Partida", command= loadgame).pack()
+tk.Button(app, text="Guardar y Salir", command= shutdown).pack()
+
+
+
+app.mainloop()
