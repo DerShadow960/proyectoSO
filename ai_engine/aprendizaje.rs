fc05776cd071f549573e545b16ac52499e8a9371
DerShadow960 <0273680@up.edu.mx>
2026-03-04 13:56:56 -0600

    Empezamos con rust :)

---
+++ ai_engine/aprendizaje.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    //Hacemos una variable, donde impirmimos espacios de lineas 20 veces, como esta en el argumento de repeat, depues imprimimos el valor de la variable con {}
+    //y luego adjuntamos la variable, ripo c#     
+    let espacios = "<<>><<>> aloHola <<>><<>>\n".repeat(15);
+    println!("Esto es un espacio: ");
+    println!("{}", espacios);
+    println!("Pruebas");
+}
+
